c8b84236bf18faddece749b48095be1112e1cae7
MissamRazaNaqvi <ljmca2021student@ljku.edu.in>
2023-06-13 17:37:56 +0530

    add pwa

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter } from 'react-router-dom';
-import './i18n.jsx' 
+import './i18n.jsx'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,3 +25,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register()
